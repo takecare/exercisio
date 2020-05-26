df12763ae023c695a7f2986658ea3fee0789e02c
takecare <rui@vazteixeira.org>
2020-05-26 15:49:15 +0100

    setup view binding

--- app/src/main/java/io/exercis/MainActivity.kt
+++ app/src/main/java/io/exercis/MainActivity.kt
@@ -1,13 +1,15 @@
 package io.exercis
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import io.exercis.R
+import androidx.appcompat.app.AppCompatActivity
+import io.exercis.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
     }
 }
