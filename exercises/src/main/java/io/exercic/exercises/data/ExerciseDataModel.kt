4d94378365d46a4a8a86ed3668d7232b3b2d654c
takecare <rui@vazteixeira.org>
2020-06-02 10:00:24 +0100

    add exercises module

---
+++ exercises/src/main/java/io/exercic/exercises/data/ExerciseDataModel.kt
@@ -0,0 +1,7 @@
+package io.exercic.exercises.data
+
+data class ExerciseDataModel(
+    val id: String, // UUID?
+    val name: String,
+    val description: String
+)
---
+++ exercises/src/main/java/io/exercic/exercises/domain/Exercise.kt
@@ -0,0 +1,6 @@
+package io.exercic.exercises.domain
+
+data class Exercise(
+    val name: String,
+    val description: String
+)
