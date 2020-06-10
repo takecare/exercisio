c4ece269743a40cf6caafa8a522a83804196652b
takecare <rui@vazteixeira.org>
2020-06-10 13:58:16 +0100

    stop providing all VM dependencies

--- workouts/src/main/java/io/exercis/workouts/ui/PresentationModule.kt
+++ workouts/src/main/java/io/exercis/workouts/ui/PresentationModule.kt
@@ -12,25 +12,25 @@ import javax.inject.Named
 @Module
 class PresentationModule {
 
-    @Provides
-    @Named("workouts")
-    @FragmentScope
-    fun provideOwner(): DelegateSavedStateRegistryOwner {
-        return DelegateSavedStateRegistryOwner()
-    }
-
-    @Provides
-    fun provideWorkoutsViewModelFactory(getWorkoutsUseCase: GetWorkoutsUseCase): ViewModelAssistedFactory<WorkoutsViewModel> {
-        return WorkoutsViewModelFactory(getWorkoutsUseCase)
-    }
-
-    @Provides
-    @FragmentScope
-    @Named("workouts")
-    fun provideWorkoutsGenericSavedStateViewModelFactory(
-        viewModelFactory: ViewModelAssistedFactory<WorkoutsViewModel>,
-        @Named("workouts") owner: DelegateSavedStateRegistryOwner
-    ): GenericSavedStateViewModelFactory<WorkoutsViewModel> {
-        return GenericSavedStateViewModelFactory(viewModelFactory, owner)
-    }
+//    @Provides
+//    @Named("workouts")
+//    @FragmentScope
+//    fun provideOwner(): DelegateSavedStateRegistryOwner {
+//        return DelegateSavedStateRegistryOwner()
+//    }
+//
+//    @Provides
+//    fun provideWorkoutsViewModelFactory(getWorkoutsUseCase: GetWorkoutsUseCase): ViewModelAssistedFactory<WorkoutsViewModel> {
+//        return WorkoutsViewModelFactory(getWorkoutsUseCase)
+//    }
+//
+//    @Provides
+//    @FragmentScope
+//    @Named("workouts")
+//    fun provideWorkoutsGenericSavedStateViewModelFactory(
+//        viewModelFactory: ViewModelAssistedFactory<WorkoutsViewModel>,
+//        @Named("workouts") owner: DelegateSavedStateRegistryOwner
+//    ): GenericSavedStateViewModelFactory<WorkoutsViewModel> {
+//        return GenericSavedStateViewModelFactory(viewModelFactory, owner)
+//    }
 }
