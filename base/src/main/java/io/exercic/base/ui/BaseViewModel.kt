596bd544a3ed8ca75ee8364787f11069b662ca69
takecare <rui@vazteixeira.org>
2020-06-12 21:22:58 +0100

    remove old base view model code

--- base/src/main/java/io/exercic/base/ui/BaseViewModel.kt
+++ base/src/main/java/io/exercic/base/ui/BaseViewModel.kt
@@ -1,11 +1,13 @@
 package io.exercic.base.ui
 
 import androidx.lifecycle.*
+import io.exercic.base.di.ViewModelAssistedFactory
 import kotlinx.coroutines.channels.BroadcastChannel
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.asFlow
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
+import javax.inject.Inject
 
 // TODO merge Effect and State? e.g. Pair<State, Effect> and effect has a 'consumed' bool
 interface State
@@ -40,7 +42,7 @@ abstract class BaseViewModel<S : State, E : Effect, Ev : Event>(
         observe(events.asFlow())
     }
 
-    fun observe(events: Flow<Ev>) {
+    fun observe(events: Flow<Ev>) { // rename to bind()? connect()?
         viewModelScope.launch {
             events.collect { handleEvent(it) }
         }
@@ -55,49 +57,4 @@ abstract class BaseViewModel<S : State, E : Effect, Ev : Event>(
     }
 
     abstract fun handleEvent(event: Ev)
-}
-
-//
-
-abstract class _BaseViewModel<EVENT, STATE, EFFECT>(setup: () -> Unit) : ViewModel() {
-
-    private val _state = MutableLiveData<STATE>()
-    val state: LiveData<STATE> get() = _state
-
-    // https://github.com/android/architecture-samples/blob/dev-todo-mvvm-live/todoapp/app/src/main/java/com/example/android/architecture/blueprints/todoapp/SingleLiveEvent.java
-    private val _effects = MutableLiveData<EFFECT>()
-    val effects: LiveData<EFFECT> get() = _effects
-
-    private val ops: MutableMap<EVENT, (EVENT) -> Any?> = mutableMapOf()
-
-    init {
-        setup()
-    }
-
-    fun observe(events: Flow<EVENT>) {
-//        viewModelScope.launch { events.collect { event -> handleEvent(event) } }
-        viewModelScope.launch { events.collect { event -> matchOp(event) } }
-    }
-
-    fun on(event: EVENT, op: (EVENT) -> Any?) {
-        ops[event] = op
-    }
-
-    fun Any?.produce(f: () -> Any?) {
-
-    }
-
-    private fun matchOp(event: EVENT) {
-        ops[event]?.let { it(event) }
-    }
-
-    // on EVENT do OPERATION
-    // produce EFFECT ?
-    // reduce STATE
-
-//    abstract fun handleEvent(event: EVENT)
-
-    fun effect(effect: EFFECT) {
-        _effects.postValue(effect)
-    }
 }
